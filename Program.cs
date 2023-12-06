4fd6395564466577f5fa0af8b1f7f287fd69bde5
SBICZA01\A253559 <Lesedi.Shirami@standardbank.co.za>
2023-12-06 08:28:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_method_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //static method is about the class itself
+            //Math is part of C# packages
+            Console.WriteLine(Math.Sqrt(144));
+
+            // UseFullTools tools = new UseFullTools() if u make the class static as well, u will only access it will direct methods?????
+
+            UseFullTools.sayHi("Lesedi");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UseFullTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_method_class
+{
+   static internal class UseFullTools
+    {
+        public static void sayHi(String name) 
+        {
+            Console.WriteLine("Hi " + name);
+        }
+    }
+}
